7722b4f7b135c770e6b38385a11ae53cb3846e30
Ken Chen <kenchenpcpm@gmail.com>
2020-01-14 10:08:56 -0800

    Initial commit

---
+++ Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp
+{
+    /// <summary>
+    /// This represent a bank account
+    /// where you can withdraw or deposit
+    /// money into the account
+    /// </summary>
+    class Account
+    {
+        #region Properties
+        public int AccountNumber { get; set; }
+        public string AccountName { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; set; }
+
+        public string EmailAddress { get; set; }
+        public DateTime CreatedDate { get; set; }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAccount = new Account();
+            myAccount.AccountName = "Yueer's account";
+
+            var myAccount2 = new Account();
+            myAccount2.AccountName = "Ken's account";
+
+        
+        }
+    }
+}
